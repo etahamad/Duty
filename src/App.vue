dfd106c9f632ab1b5ccae45da93f6a197354c7ed
Omar Hamad <etatldr@gmail.com>
2024-03-27 00:40:17 +0200

    Add github link

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@ import EgyptHolidays from './components/EgyptHolidays.vue'
 
 <template>
   <div id="app">
+    <a href="https://github.com/etahamad/DutyFree" target="_blank" class="github-link">
+      <i class="fab fa-github"></i>
+      <span>View on GitHub</span>
+    </a>
     <Countdown format="days" label="Days left:" />
     <Countdown format="months-days" label="Months and days left:" />
     <hr class="divider" />
@@ -18,5 +22,19 @@ import EgyptHolidays from './components/EgyptHolidays.vue'
   border: none;
   border-top: 1px solid #ccc;
   margin: 20px 0;
+}
+.github-link {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: inherit;
+}
+.github-link span {
+  margin-top: 5px;
+}
+.fab {
+  font-size: 24px;
+  color: #000;
 }
 </style>
